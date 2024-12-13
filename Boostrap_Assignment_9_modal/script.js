b6e2a2e5705b402ffbe88a3a2be67a8d6d9f8f6d
chaity345 <155262153+chaity345@users.noreply.github.com>
2024-12-12 20:45:39 -0500

    Add files via upload

---
+++ Boostrap_Assignment_9_modal/script.js
@@ -0,0 +1,11 @@
+function showResult(){
+    let quantity= prompt("How many orders would like to add to Shopping Cart");
+    let price=9;
+    let totalPrice=quantity*price;
+    alert(`${quantity} has been received!The total price is ${totalPrice}`);
+
+    
+
+} 
+    
+
